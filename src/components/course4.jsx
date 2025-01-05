6914bf5a2d88e37dd6f5e53c4271fa6c14ebfec6
steadfast <achal02gupta@gmail.com>
2025-01-05 17:30:46 +0530

    created Todo Application

--- src/components/course4.jsx
+++ src/components/course4.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import MiniProject32 from "./course4/miniProject32";
+import TodoApp34 from "./course4/todoApp34";
 
 const Course4 = () => {
   return (
     <>
-      <MiniProject32 />
+      {/* <MiniProject32 /> */}
+
+      <TodoApp34 />
     </>
   );
 };
---
+++ src/components/course4/todoApp34.jsx
@@ -0,0 +1,94 @@
+import React, { useEffect, useState } from "react";
+import { MdCheck, MdDeleteForever } from "react-icons/md";
+import "./todoApp.css";
+
+const TodoApp34 = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [task, setTask] = useState([]);
+  const [currDate, setCurrDate] = useState("");
+
+  const handleInputChange = (value) => {
+    setInputValue(value);
+  };
+
+  const handleSubmitForm = (event) => {
+    event.preventDefault();
+    if (!inputValue) return;
+    if (task.includes(inputValue)) {
+      setInputValue("");
+      return;
+    }
+    setTask((prevTask) => [...prevTask, inputValue]);
+    setInputValue("");
+  };
+
+  // console.log("Hello");
+
+  //   setInterval(() => {
+  //     const now = new Date();
+  //     const formatedDate = now.toLocaleDateString();
+  //     const formatedTime = now.toLocaleTimeString();
+  //     setCurrDate(`${formatedDate} - ${formatedTime}`);
+  //   }, 1000);
+
+//   console.log("Hello");
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const now = new Date();
+      const formatedDate = now.toLocaleDateString();
+      const formatedTime = now.toLocaleTimeString();
+      setCurrDate(`${formatedDate} - ${formatedTime}`);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <section className="todo-container">
+      <header>
+        <h1>Todo List</h1>
+        <h3>{currDate}</h3>
+      </header>
+      <section className="form">
+        <form onSubmit={handleSubmitForm}>
+          <div>
+            <input
+              type="text"
+              className="todo-input"
+              autoComplete="off"
+              value={inputValue}
+              onChange={(e) => {
+                handleInputChange(e.target.value);
+              }}
+              name=""
+              id=""
+            />
+          </div>
+          <div>
+            <button type="submit" className="todo-btn">
+              Add Task
+            </button>
+          </div>
+        </form>
+      </section>
+      <section className="myUnOrderList">
+        <ul>
+          {task.map((curTask, index) => {
+            return (
+              <li key={index} className="todo-items">
+                <span>{curTask}</span>
+                <button className="check-btn">
+                  <MdCheck />
+                </button>
+                <button className="delete-btn">
+                  <MdDeleteForever />
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      </section>
+    </section>
+  );
+};
+
+export default TodoApp34;
