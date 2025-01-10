dfd35d961688de2cff848d8330d41ba9c3999106
steadfast <achal02gupta@gmail.com>
2025-01-10 14:09:11 +0530

    added useReducer concept in course8

--- src/components/course8/hook_useReducer69.jsx
+++ src/components/course8/hook_useReducer69.jsx
@@ -1,31 +1,40 @@
 import React, { useReducer } from "react";
 
 const Hook_useReducer69 = () => {
-  const reducer = (state, action) => {
+  const countReducer = (state, action) => {
     if (action.type === "Increment") {
       return state + 1;
     }
     if (action.type === "Decrement") {
       return state - 1;
     }
+    if (action.type === "Reset") {
+      return (state = 0);
+    }
   };
 
-  const [count, dispatch] = useReducer(reducer, 0);
+  const [count, countDispatch] = useReducer(countReducer, 0);
   return (
     <div>
       <h1>Use Reducer</h1>
       <span>{count}</span>
       <button
-        onClick={() => dispatch({ type: "Increment" })}
+        onClick={() => countDispatch({ type: "Increment" })}
         style={{ width: "10rem" }}
       >
         Increment
       </button>
       <button
-        onClick={() => dispatch({ type: "Decrement" })}
+        onClick={() => countDispatch({ type: "Decrement" })}
         style={{ width: "10rem" }}
       >
         Decrement
+      </button>
+      <button
+        onClick={() => countDispatch({ type: "Reset" })}
+        style={{ width: "10rem" }}
+      >
+        Reset
       </button>
     </div>
   );
