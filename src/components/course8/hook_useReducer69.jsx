aace3c9f42bce263df7e2a78f4c3b994c2caf8b5
steadfast <achal02gupta@gmail.com>
2025-01-10 14:18:39 +0530

    added new method in usereducer in course 8

--- src/components/course8/hook_useReducer69.jsx
+++ src/components/course8/hook_useReducer69.jsx
@@ -1,23 +1,43 @@
 import React, { useReducer } from "react";
 
 const Hook_useReducer69 = () => {
+  const initialState = {
+    count: 0,
+    inc: 2,
+    dec: 5,
+  };
+  //   const countReducer = (state, action) => {
+  //     if (action.type === "Increment") {
+  //       return state + 1;
+  //     }
+  //     if (action.type === "Decrement") {
+  //       return state - 1;
+  //     }
+  //     if (action.type === "Reset") {
+  //       return (state = 0);
+  //     }
+  //   };
+
   const countReducer = (state, action) => {
-    if (action.type === "Increment") {
-      return state + 1;
-    }
-    if (action.type === "Decrement") {
-      return state - 1;
-    }
-    if (action.type === "Reset") {
-      return (state = 0);
+    switch (action.type) {
+      case "Increment":
+        return { ...state, count: state.count + 1 };
+      case "Decrement":
+        return { ...state, count: state.count - 1 };
+      case "Reset":
+        return { ...state, count: 0 };
+      default:
+        return state;
     }
   };
 
-  const [count, countDispatch] = useReducer(countReducer, 0);
+  //   const [count, countDispatch] = useReducer(countReducer, 0);
+  const [state, countDispatch] = useReducer(countReducer, initialState);
+
   return (
     <div>
       <h1>Use Reducer</h1>
-      <span>{count}</span>
+      <span>{state.count}</span>
       <button
         onClick={() => countDispatch({ type: "Increment" })}
         style={{ width: "10rem" }}
